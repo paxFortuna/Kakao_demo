b8712ee2b43bb42bab11bbb15b7f0237c235cb1c
fortuna <avidia38@gmail.com>
2022-07-07 11:16:36 +0900

    carousel error

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,16 +1,33 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+
+final dummyItems =[
+  "kakao_demo/assets/imgs/chat_on.png",
+  "kakao_demo/assets/imgs/home_off.png",
+  "kakao_demo/assets/imgs/home_on.png",
+  "kakao_demo/assets/imgs/location_off.png",
+  "kakao_demo/assets/imgs/note_on.png",
+];
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
-        const SizedBox(height: 20,),
+        const SizedBox(
+          height: 20,
+        ),
         _buildTop(),
-        // _buildMiddle(),
-        // _buildBottom(),
+        const SizedBox(
+          height: 20,
+        ),
+        _buildMiddle(),
+        const SizedBox(
+          height: 20,
+        ),
+        _buildBottom(),
       ],
     );
   }
@@ -22,64 +39,93 @@ class HomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             GestureDetector(
+              onTap: () {
+                print("호출");
+              },
+              child: Column(
+                children: const [
+                  Icon(Icons.local_taxi, color: Colors.white, size: 40),
+                  Text(
+                    "택시",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                print("호출");
+              },
+              child: Column(
+                children: const [
+                  Icon(Icons.local_taxi, color: Colors.white, size: 40),
+                  Text(
+                    "대리",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                print("호출");
+              },
+              child: Column(
+                children: const [
+                  Icon(Icons.local_taxi, color: Colors.white, size: 40),
+                  Text("모범", style: TextStyle(color: Colors.white)),
+                ],
+              ),
+            ),
+            GestureDetector(
               onTap: (){
                 print("호출");
               },
               child: Column(
                 children: const [
                   Icon(Icons.local_taxi, color: Colors.white, size: 40),
-                  Text("택시", style: TextStyle(color: Colors.white),),
+                  Text("블랙", style: TextStyle(color: Colors.white)),
                 ],
               ),
-            ),
-            Column(
-              children: const [
-                Icon(Icons.local_taxi, color: Colors.white, size: 40),
-                Text("대리", style: TextStyle(color: Colors.white),),
-              ],
-            ),
-            Column(
-              children: const [
-                Icon(Icons.local_taxi, color: Colors.white,size: 40),
-                Text("모범",style: TextStyle(color: Colors.white)),
-              ],
-            ),
-            Column(
-              children: const [
-                Icon(Icons.local_taxi, color: Colors.white,size: 40),
-                Text("블랙",style: TextStyle(color: Colors.white)),
-              ],
             )
           ],
         ),
-        const SizedBox(height: 20,),
+        const SizedBox(
+          height: 20,
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Column(
               children: const [
                 Icon(Icons.local_taxi, color: Colors.white, size: 40),
-                Text("승합", style: TextStyle(color: Colors.white),),
+                Text(
+                  "승합",
+                  style: TextStyle(color: Colors.white),
+                ),
               ],
             ),
             Column(
               children: const [
                 Icon(Icons.local_taxi, color: Colors.white, size: 40),
-                Text("미니", style: TextStyle(color: Colors.white),),
+                Text(
+                  "미니",
+                  style: TextStyle(color: Colors.white),
+                ),
               ],
             ),
             Column(
               children: const [
-                Icon(Icons.local_taxi, color: Colors.white,size: 40),
-                Text("우버",style: TextStyle(color: Colors.white)),
+                Icon(Icons.local_taxi, color: Colors.white, size: 40),
+                Text("우버", style: TextStyle(color: Colors.white)),
               ],
             ),
             Opacity(
               opacity: 0.0,
               child: Column(
                 children: const [
-                  Icon(Icons.local_taxi, color: Colors.white,size: 40),
-                  Text("택시",style: TextStyle(color: Colors.white)),
+                  Icon(Icons.local_taxi, color: Colors.white, size: 40),
+                  Text("택시", style: TextStyle(color: Colors.white)),
                 ],
               ),
             )
@@ -89,11 +135,41 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  // Widget _buildMiddle() {
-  //   return null;
-  // }
-  //
-  // Widget _buildBottom() {
-  //   return null;
-  // }
+Widget _buildMiddle() {
+  return CarouselSlider(
+      options: CarouselOptions(
+        height: 200,
+        aspectRatio: 16/9,
+        autoPlay: false,
+      ),
+    items: dummyItems.map((img){
+      return Builder(builder: (BuildContext context){
+        return Container(
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.symmetric(horizontal: 5.0),
+          child: ClipRect(
+              child: Image.asset(
+                img,
+                fit: BoxFit.fill,
+             ),
+           ),
+        );
+        },
+      );
+    }).toList(),
+  );
+}
+
+Widget _buildBottom() {
+    final items = List.generate(10, (i) => const ListTile(
+      leading: Icon(Icons.notifications_none),
+      title: Text("[이벤트] 이것은 공지사항입니다."),
+    ),
+    );
+    return ListView(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+        children: items,
+    );
+  }
 }
