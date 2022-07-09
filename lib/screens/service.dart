a7d1ebab8d7f257b32df7df1f791bb39a3f59018
fortuna <avidia38@gmail.com>
2022-07-09 21:27:05 +0900

    service_screen appBar

--- lib/screens/service.dart
+++ lib/screens/service.dart
@@ -5,10 +5,17 @@ class ServiceScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-      "이용서비스 페이지",
-      style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("이용서비스"),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          "이용서비스 페이지",
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
         ),
-    );}
+      ),
+    );
+  }
 }
